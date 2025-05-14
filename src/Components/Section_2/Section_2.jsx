137510b9f3c54051233b522837739c49215bd275
Julian <jacaminaur@gmail.com>
2025-05-14 10:22:56 +0200

    Section 2

--- src/Components/Section_2/Section_2.jsx
+++ src/Components/Section_2/Section_2.jsx
@@ -3,17 +3,26 @@ import desktopInteractiveIMG from "../../assets/images/mobile/image-interactive.
 
 function Section_2() {
   return (
-    <div className="h-screen flex flex-col items-center px-8 py-12">
-      <img className="" src={mobileInteractiveIMG} alt="" />
-      <p className="uppercase text-4xl md:text-4xl lg:text-5xl p-4 text-center font-light mt-8">
-        The leader in interactive VR
-      </p>
-      <p className="font-medium text-center text-gray-400 text-lg md:text-xl lg:text-2xl px-4 mt-4">
-        Founded in 2011, Loopstudios has been producing world-class virtual
-        reality projects for some of the best companies around the globe. Our
-        award-winning creations have transformed businesses through digital
-        experiences that bind to their brand.
-      </p>
+    <div className="relative flex flex-col md:flex-row items-center justify-center px-8 py-12 md:px-24 h-screen md:h-screen ">
+      {/* Imagen */}
+      <img
+        className="md:absolute left-10 w-230 max-w-[80%]"
+        src={mobileInteractiveIMG}
+        alt=""
+      />
+
+      {/* Caja de texto */}
+      <div className="bg-white flex flex-col justify-center items-center md:h-1/2 md:absolute md:bottom-0 p-6 gap-6 md:w-150 md:right-0 lg:right-24 lg:w-1/2 xl:h-120">
+        <p className="uppercase text-4xl md:text-4xl lg:text-5xl text-center md:text-left font-light mb-6 mx-10 w-110 lg:w-120">
+          The leader in interactive VR
+        </p>
+        <p className="font-medium text-center md:text-left text-gray-400 text-lg md:text-lg mx-10 w-110 lg:w-120">
+          Founded in 2011, Loopstudios has been producing world-class virtual
+          reality projects for some of the best companies around the globe. Our
+          award-winning creations have transformed businesses through digital
+          experiences that bind to their brand.
+        </p>
+      </div>
     </div>
   );
 }
